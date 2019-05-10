3e1a922f6b5c604116e43ec050de614da6498d29
Angelika Cimene <angelikacimene@gmail.com>
2019-05-10 11:58:26 -0700

    homepage
    revised

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 'use strict';
 console.log('reading js');
 
+
+var hamburger = document.querySelector('#open');
+var hamburgerClose = document.querySelector('#close');
+
+
 var mobileReports = document.querySelector('#mobileRep');
 var mobileAbout = document.querySelector('#mobileAbt');
 var mobileContact = document.querySelector('#mobileContact');
+
+
+
+// Mobile Navigation Click
+hamburger.addEventListener('click', function() {
+  document.querySelector('#mobileNav').style.display = 'block';
+  document.querySelector('#open').style.display = 'none';
+  document.querySelector('#close').style.display = 'flex';
+});
+
+hamburgerClose.addEventListener('click', function() {
+  document.querySelector('#mobileNav').style.display = 'none';
+  document.querySelector('#open').style.display = 'block';
+  document.querySelector('#close').style.display = 'none';
+});
 
 // Mobile Navigation Hovers For Submenu
 mobileReports.addEventListener('mouseover', function() {
